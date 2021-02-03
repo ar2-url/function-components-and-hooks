8da3a8232876f0cd179ab9468afb72883f27d6ca
ar2-url <57313716+ar2-url@users.noreply.github.com>
2021-02-03 20:16:52 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+//this file is to task app
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./Container/AppClass";
+// import App from "./Container/AppFunction";
+
+ReactDOM.render(
+  <App />,
+  document.getElementById("app")
+);
